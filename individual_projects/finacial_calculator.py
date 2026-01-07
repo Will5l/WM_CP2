b5a1ff5f4d84f64946750a719d0617c68608c02d
Will5l <Will.malloch@ucas-edu.net>
2026-01-07 19:46:22 +0000

    psuedo

--- individual_projects/finacial_calculator.py
+++ individual_projects/finacial_calculator.py
@@ -3,4 +3,10 @@
 
 #Greet them to the calculator
 
-#
+#Make a main function that will be where everything is done, like choosing what to do.
+    #Have one option for depositing and ask if the deposits are monthly or weekly.
+    #It will calculate how long it would take by dividing the saving goal, by the deposits.
+
+    #If they choose the second option, it will ask them for a starting amount for the compound intrest, the intrest rate, and how many months.
+    
+    #the third option is the budget calculator, and would ask for them to give up to 5 different categories for spending   
