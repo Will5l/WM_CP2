4207d6eac5cc6b96ab14e60f0655043845a40dd6
Will5I <will.malloch@ucas-edu.net>
2026-01-13 09:52:33 -0700

    testing things with tkinter

---
+++ individual_projects/personal_library.py
@@ -0,0 +1,13 @@
+# WM 1st Personal Library
+
+import tkinter as tk
+
+
+
+
+button.bind
+def interface():
+    user = input("What is your name?\n")
+    print(f"Welcome to your personal library, {user}")
+    while True:
+        pass
