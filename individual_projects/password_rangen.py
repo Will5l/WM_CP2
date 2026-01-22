963923c76c5667c5700d0a8649b79987fe304920
Will5I <will.malloch@ucas-edu.net>
2026-01-22 09:51:54 -0700

    main made

--- individual_projects/password_rangen.py
+++ individual_projects/password_rangen.py
@@ -1,3 +1,20 @@
 # WM 1st Random Password Generator
 
 import sys
+
+def main():
+    print("Welcome to the random password generator.")
+    while True:
+        choice = input("What would you like to do?\n1.Generate password\n2.Exit")
+        if choice == '1':
+            password_gen()
+        elif choice == '2':
+            print("Goodbye")
+            sys.exit()
+        else:
+            print("Invalid, try again")
+
+def password_gen():
+    length = input("How long do you want the password?\n")
+    uppercase = input("Do you want uppercase letters?y/n\n")
+    lowercase = input("Do you want lowercase letters?y/n\n")
