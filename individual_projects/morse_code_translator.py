2ae07521bb467d394da9731f3083604f049f45a5
Will5l <Will.malloch@ucas-edu.net>
2026-02-03 06:50:39 +0000

    morse code done

--- individual_projects/morse_code_translator.py
+++ individual_projects/morse_code_translator.py
@@ -2,7 +2,7 @@
 
 import sys
 
-
+#Main function to run the interface
 def main():
     while True:
         english = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
@@ -17,21 +17,50 @@ def main():
             print("Goodbye")
             sys.exit()
 
-
+#Function that takes in . and - as morse code and translates it to english
 def morToEng(e,m):
-    morse = input("Input the morse code you have, for dots use periods, for dashes use hyphens (-) for spaces just use spaces. Make sure every letter has a space after\n")
-    for x in morse:
+    prnt = False
+    while prnt == False:
+        prnt = True
+        morse = input("Input the morse code you have, for dots use periods, for dashes use hyphens (-) for spaces just use spaces(do not include the letter spaces as actual spaces). Make sure every letter has a space after\n")
+        word = ""
         y = ''
-        y = y+x
-        if y in m:
-            
-            letter = 
-            word = ""
-            word = word+letter
-    print(word)
+        for x in morse:
+            y = y+x
+            if y == ' ':
+                word = word+' '
+                y = ''
+                continue
+            elif y in m:
+                z = m.index(y)
+                letter = e[z]
+                word = word+letter
+                y = ''
+                continue
+            elif x != ' ' and x != '-' and x != '.':
+                print("Something was invalid, try again")
+                prnt = False
+                break
+        print(word)
 
     pass
-
+#Function that takes in english and turns it into the corresponding . and - combo for morse code.
 def engToMor(e,m):
-    pass
+        prnt = False
+        while prnt == False:
+            prnt = True
+            words = input("Type what you would like to translate to morse code (use all caps)\n")
+            morse = ''
+            for x in words:
+                if x == " ":
+                    morse = morse+" "
+                elif x in e:
+                    y = e.index(x)
+                    morse = morse+m[y]
+                else:
+                    print("Something was invalid, try again")
+                    prnt = False
+                    break
+                
+        print(morse)
 main()
