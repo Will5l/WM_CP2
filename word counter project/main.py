2232564959f9dd852be5f65f706b784b4709f973
Will5l <will.malloch@ucas-edu.net>
2026-02-20 09:53:09 -0700

    personal

---
+++ word counter project/main.py
@@ -0,0 +1,4 @@
+import time_checking
+import file_updater
+def main():
+    print("Welcome to the word counter. You can write and save to a document here, and update the word count, as well as the time it was updated.")
